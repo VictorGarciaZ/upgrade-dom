711d4a5a5b173aa6c84a093bef9612e3ff302817
victorgarciaz <victor.garcia@bootcamp-upgrade.com>
2022-12-20 09:24:24 +0100

    iteracion1.js

---
+++ Iteración1.js
@@ -0,0 +1,26 @@
+/*1.1 Usa querySelector para mostrar por consola el botón con la clase .showme*/
+const showMe = document.querySelector("showme");
+console.log(showMe);
+
+/*1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado*/
+const pillado = document.querySelector("#pillado");
+console.log(pillador);
+
+
+/*1.3 Usa querySelector para mostrar por consola todos los p*/
+const parragraph = document.querySelector("p");
+console.log(parragraph);
+
+/*1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon*/
+const classPokemon = document.querySelector (".pokemon")
+console.log(classPokemon);
+
+/*1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo 
+data-function="testMe"*/
+const attr = document.querySelector("data-function="testMe") DUDA EN ESTE EJERCICIO 
+console.log(attr);
+
+/*1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
+data-function="testMe"*/
+const attr = document.querySelector ("data-function="testMe") DUDO EN ESTE EJERCICIO    
+console.log(attr[2]); 
---
+++ iteracion2.js
@@ -0,0 +1,44 @@
+/*2.1 Inserta dinamicamente en un html un div vacio con javascript.*/
+const div1 = document.createElement("div")
+document.body.appendChild(div1);
+
+/*2.2 Inserta dinamicamente en un html un div que contenga una p con javascript.*/
+const divP = document.createElement("div");
+const parragraph = document.createElement ("p");
+document.body.appendChild(divP);
+document.body.appendChild(parragraph);
+
+/*2.3 Inserta dinamicamente en un html un div que contenga 6 p utilizando un loop con javascript.*/
+const div6p = document.createElement("div");
+
+document.body.appendChild(div6p);
+
+for (let i = 0; i < 6; i++) {
+  let divSixP = document.createElement("p");
+  createDivWith6P.appendChild(divSixP);
+}
+
+/*2.4 Inserta dinamicamente con javascript en un html una p con el texto 'Soy dinámico!'.*/
+const textP = document.createElement("p");
+textP.textContent = "Soy dinámico!";
+document.body.appendChild(textP);
+
+
+/*2.5 Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.*/
+const h2Class = document.querySelector(".fn-insert-here")
+h2Class.textContent = "Wubba Lubba dub dub";
+
+
+/*2.6 Basandote en el siguiente array crea una lista ul > li con los textos del array.
+const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];*/
+const list = document.createElement ("ul") NO SE COMO SEGUIR
+
+
+/*2.7 Elimina todos los nodos que tengan la clase .fn-remove-me*/
+
+
+/*2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div. 
+	Recuerda que no solo puedes insertar elementos con .appendChild.*/
+
+
+/*2.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase .fn-insert-here*/
